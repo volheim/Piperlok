082e6fa3079b47e497bf8bcad66f5204d09819b7
volheim <aldanv@live.dk>
2017-11-07 12:14:52 +0100

    Project start

---
+++ Piperlok/Actors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Piperlok
+{
+    abstract class Actors
+    {
+
+        public bool alive;
+
+        public float speed;
+        public int health;
+
+        public string name;
+
+        public PointF position;
+        public Graphics sprite;
+
+        public abstract void Update();
+
+        public abstract void Movement();
+
+        public virtual void Collide()
+        {
+
+        }
+
+        public virtual void Gravity()
+        {
+
+        }
+
+    }
+}
