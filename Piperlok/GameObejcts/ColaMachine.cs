8b5f13e1a7ad4ed98806b3f1ba640d53437ef372
Mikkel Richter <mikkel.rr@hotmail.com>
2017-11-09 11:08:19 +0100

    Fuck off sourceControl

--- Piperlok/GameObejcts/ColaMachine.cs
+++ Piperlok/GameObejcts/ColaMachine.cs
@@ -11,6 +11,7 @@ namespace Piperlok.GameObejcts
 {
     class ColaMachine : Objects , Ipower
     {
+        private int ColasSpawned;
         public ColaMachine(bool moveable, bool power, bool dangerous, Graphics sprite, PointF position, string name, float weight) : base(moveable, power, dangerous, sprite, position, name, weight)
         {
         }
@@ -21,7 +22,10 @@ namespace Piperlok.GameObejcts
         }
         public void SpawnCola()
         {
-
+            if(ColasSpawned <= 0)
+            {
+                //new Cola(new PointF(10,10)a);
+            }
         }
     }
 }
