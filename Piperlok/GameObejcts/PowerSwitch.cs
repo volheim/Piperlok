6bd7ac8f2570847cfff597babdb442630b59a9b3
Mikkel Richter <mikkel.rr@hotmail.com>
2017-11-09 13:41:31 +0100

    Refixes

---
+++ Piperlok/GameObejcts/PowerSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Piperlok.Interfaces;
+
+namespace Piperlok.GameObejcts
+{
+    class PowerSwitch : Object, Ipower
+    {
+        public void PowerOn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
